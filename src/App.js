f8d5e8f56dd50097d67defc958b00df4d92fa67f
Craig Smith <craigmarksmith@gmail.com>
2019-11-04 13:47:57 +1100

    get the stock price and add to the portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   return (
@@ -13,42 +14,87 @@ class PageComponent extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = { balance: localStorage.getItem('balance')}
-    this.addMoney = this.addMoney.bind(this)
+    this.state = { 
+      balance: localStorage.getItem('balance'),
+      stockForm: {
+        stockCode: "",
+        qty: ""
+      },
+      portfolio: [],
+
+    }
+    this.addToBalance = this.addToBalance.bind(this)
     this.getBalance = this.getBalance.bind(this)
     this.setStockNameInput = this.setStockNameInput.bind(this)
     this.setQtyInput = this.setQtyInput.bind(this)
     this.buyStock = this.buyStock.bind(this)
 }
 
-  addMoney(qty) {
-    let currentBalance = this.state['balance']
-    let newBalance = currentBalance + qty
+  addToBalance(amount) {
+    const currentBalance = this.state['balance']
+    const newBalance = currentBalance + amount
     this.setState({balance: newBalance})
     localStorage.setItem('balance', this.state['balance'])
   }
   
   getBalance() {
-    let balanceInCent = this.state['balance']
+    const balanceInCent = this.state['balance']
     let balanceInDollars = balanceInCent / 100
     balanceInDollars = balanceInDollars.toLocaleString("en-AU", {style:"currency", currency:"AUD"});
     return balanceInDollars
   }
 
+  subtractFromBalance(amount) {
+    const currentBalance = this.state['balance']
+    const newBalance = currentBalance - amount
+    this.setState({balance: newBalance})
+    localStorage.setItem('balance', this.state['balance'])
+  }
+
+  addToPortfolio(stockName, qty, unitPrice) {
+    const newStock = {
+      name: stockName,
+      qty: qty,
+      unitPrice: unitPrice
+    }
+    let portfolio = this.state.portfolio
+    portfolio.push(newStock)
+    // this.state = {
+    //   ...this.state, portfolio: portfolio
+    // }
+  }
+
   setStockNameInput(e) {
-    let newStockForm = { ...this.state.stockForm, stockCode: e.target.value }
+    const newStockForm = { ...this.state.stockForm, stockCode: e.target.value }
     this.setState({ ...this.state, stockForm: newStockForm })
   }
 
   setQtyInput(e) {
-    let newStockForm = { ...this.state.stockForm, qty: e.target.value }
+    const newStockForm = { ...this.state.stockForm, qty: e.target.value }
     this.setState({ ...this.state, stockForm: newStockForm })
   }
 
-  buyStock() {
-    console.log("Buy stock")
-    console.log(this.state.stockForm.stockCode)
-    console.log(this.state.stockForm.qty)
+  async buyStock() {
+    //get cost for stock
+    let response
+    try {
+      response = await axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=demo')
+    } catch {
+      console.log("Error, something went wrong")
+    }
+    let timeSeriesData = response.data["Time Series (1min)"]
+    let unitPrice = timeSeriesData[Object.keys(timeSeriesData)[0]]["4. close"]
+
+    const priceInCent = unitPrice * 100
+    const totalPrice = priceInCent * this.state.stockForm.qty
+
+    //reduce from balance
+    this.subtractFromBalance(totalPrice)
+
+    //add to portfolio
+    this.addToPortfolio(this.state.stockForm.stockCode, this.state.stockForm.qty, unitPrice)
+
+    //say you did it!
   }
 
   render() {
@@ -56,7 +102,7 @@ class PageComponent extends React.Component {
       <div>
         <div>
           <h2>Current balance: {this.getBalance()}</h2>
-          <p>Add money: <button onClick={() => this.addMoney(1000)}>Add $10</button></p>
+          <p>Add money: <button onClick={() => this.addToBalance(1000)}>Add $10</button></p>
         </div>
         <div>
           <h2>Portfolio:</h2>
